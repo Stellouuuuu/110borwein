851c9fdbe4da6b0d9c83eb1c13d60b503540c97b
Stellouuuuu <stella.gbaguidi@epitech.eu>
2025-04-25 13:15:18 +0000

    Synced folder from Repo A fork

--- borwein.js
+++ borwein.js
@@ -1,9 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** B-MAT-200-COT-2-1-110borwein-stella.gbaguidi
+** File description:
+** borwein
+*/
+
 function ver_val_abs(x)
 {
     if (Math.abs(x) < 1e-8)
         return 1.0;
     if (x != 0)
-        return asin(x) / x;
+        return Math.sin(x) / x;
     else
         return 0;
 }
@@ -24,8 +31,11 @@ function function_evaluation(x, n)
     return f;
 }
 
-function midpoint(a, b, n)
+function midpoint()
 {
+    let a = 0;
+    let b = 5000;
+    let n = parseInt(document.getElementById("n").value);
     let tmp = 0.0;
     let f = 0.0;
     let h = Number(b - a) / 10000;
@@ -35,17 +45,18 @@ function midpoint(a, b, n)
         f += function_evaluation(tmp, n);
     }
     f = f * h;
-    process.stdout.write("Midpoint:\n");
-    process.stdout.write("I%d = %.10f\n");
-    process.stdout.write(n);
-    process.stdout.write(f);
-    process.stdout.write("diff = %.10f\n\n");
-    process.stdout.write((M_PI / 2) - f);
-    return f;
+    result = "Midpoint:\n"
+    result += `I${n} = ${f.toFixed(10)}\n`
+    result += `diff${n} = ${3.1415926535 / 2}`
+    document.getElementById("mid").innerText = result;
+    return result;
 }
 
-function trapezoidal(a, b, n)
+function trapezoidal()
 {
+    let a = 0;
+    let b = 5000;
+    let n = parseInt(document.getElementById("n").value);
     let tmp = 0.0;
     let f = 0.5 * (function_evaluation(a, n) + function_evaluation(b, n));
     let h = Number(b - a) / 10000;
@@ -55,13 +66,42 @@ function trapezoidal(a, b, n)
         f += function_evaluation(tmp, n);
     }
     f = f * h;
-    process.stdout.write("Trapezoidal:\n");
-    process.stdout.write("I%d = %.10f\n");
-    process.stdout.write(n);
-    process.stdout.write(f);
-    process.stdout.write("diff = %.10f\n\n");
-    process.stdout.write((M_PI / 2) - f);
-    return f;
+    result = "Trapezoidal:\n"
+    result += `I${n} = ${f.toFixed(10)}\n`
+    result += `diff${n} = ${3.1415926535 / 2}`
+    document.getElementById("trap").innerText = result;
+    return result;
 }
+function simpson()
+{
+    let a = 0;
+    let b = 5000;
+    let n = parseInt(document.getElementById("n").value);
+    let r = 0;
+    let x = 0;
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    let l = 0;
+    let m = 0;
+    let o = 0;
 
-midpoint(0, 5000, process.argv[2])
+    a = 0;
+    b = 0.5;
+    while (b <= 5000) {
+        i = function_evaluation(a, n);
+        j = function_evaluation(b, n);
+        k = function_evaluation((a + b) / 2, n);
+        l = i + (4 * k) + j;
+        m = (b - a) / 6;
+        o = m * l;
+        r += o;
+        a += 0.5;
+        b += 0.5;
+    }
+    result = "Simpson:\n"
+    result += `I${n} = ${r.toFixed(10)}\n`
+    result += `diff${n} = ${3.1415926535 / 2}`
+    document.getElementById("simp").innerText = result;
+    return result;
+}
